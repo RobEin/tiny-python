97ba44dbecc4302ddd79488d60c0b1888dec6938
Robert Einhorn <robert.einhorn.hu@gmail.com>
2019-12-01 15:49:56 +0100

    tiny Python

--- grammar without actions/LexerWithIndentDedentInjector.java
+++ grammar without actions/LexerWithIndentDedentInjector.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2019 by Robert Einhorn
+ * Copyright (c) 2019 Robert Einhorn
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
