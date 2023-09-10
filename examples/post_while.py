60165deb68579d875246dd5147eddcc4aff98de5
Robert Einhorn <robert.einhorn.hu@gmail.com>
2023-09-10 11:08:15 +0200

    Update post_while.py

--- examples/post_while.py
+++ examples/post_while.py
@@ -2,6 +2,6 @@ x = 0
 while 1 == 1:
     x = x + 1
     print(x)
-    if input("Press the 'n' key with an 'Enter' to print the next number or any other key to end the program.") != "n":
+    if input("Press the 'n' key with an 'Enter' to print the next number or press any other key to end the program.") != "n":
         break
 print("Writing numbers finished.")
